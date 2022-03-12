0df3f4d7fc2ca48a5a24ea87fd7b22856ee157e9
rawand1998 <rawandgaradh@gmail.com>
2022-03-12 19:06:48 +0200

    style files

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,11 @@
 <div class="conatiner">
     <div class="row">
          <div class="col-sm-4 col-sm-offset-4">
-         <div class="card" style="width: 18rem;">
+         <div class="card card-post">
 @foreach($posts as $post)
   <div class="card-body">
-    <h5 class="card-title">{{$post->title}}</h5>
-    <p class="card-text">{{$post->body}}</p>
+    <h5 class="card-title title">{{$post->title}}</h5>
+    <p class="card-text body">{{$post->body}}</p>
     <a href="/details/{{$post->id}}" class="btn btn-primary">Read More</a>
     @endforeach
   </div>
