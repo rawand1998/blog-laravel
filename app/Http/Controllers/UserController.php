ae73c0d61701c03314337a60ddf2adfbf9ea9459
rawand1998 <rawandgaradh@gmail.com>
2022-03-12 13:40:02 +0200

    edit navbar after register

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,4 +17,12 @@ class UserController extends Controller
             return redirect('/');
         }
     }
+    public function register(Request $request){
+        $user = new User;
+        $user->name=$request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+        return redirect('/');
+    }
 }
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -22,9 +22,20 @@
       </form>
       </ul>
       <ul class="nav navbar-nav navbar-right">
-        <li> <a href="">Login</a>
-        <li> <a href="">register</a>
+          @if(Session::get('user'))
+          <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">{{Session::get('user')['name']}}
+            <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="/logout">Logout</a></li>
+            </ul>
+          </li>
+          @else
+          <li> <a href="/login">Login</a>
+        <li> <a href="/register">register</a>
             </li>
+          @endif
+     
     </ul>
     </div>
   </div>
